f66dc485cefb1b673257f53449e8f914fe13ab92
nmFaizz <ilznorz582@gmail.com>
2024-07-08 14:44:07 +0700

    feat: radius

--- src/components/container/homepage/hero-section/HeroSection.tsx
+++ src/components/container/homepage/hero-section/HeroSection.tsx
@@ -5,7 +5,7 @@ import ButtonLink from "@/components/Buttons/ButtonLink";
 
 export default function HeroSection() {
     return (
-        <section className="bg-berkeley-blue min-h-0 md:min-h-[75vh] rounded-[15px] lg:rounded-[28px] gap-12 md:gap-24 flex lg:flex-row flex-col items-center justify-between px-[30px] xl:px-[60px] py-[46px]">
+        <section className="bg-berkeley-blue min-h-0 md:min-h-[75vh] rounded-[15px] xl:rounded-[28px] gap-12 md:gap-24 flex lg:flex-row flex-col items-center justify-between px-[30px] xl:px-[60px] py-[46px]">
             <div className="flex-1 space-y-5 flex flex-col items-center lg:items-start gap-5">
                 <div className="text-white-primary space-y-5">
                     <Typography weight="regular">
