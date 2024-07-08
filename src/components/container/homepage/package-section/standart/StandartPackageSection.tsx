fc401d08b1cea9f8c104ce1ab3f19a2c793b1463
nmFaizz <ilznorz582@gmail.com>
2024-07-08 14:16:03 +0700

    feat: bit adjusment

--- src/components/container/homepage/package-section/standart/StandartPackageSection.tsx
+++ src/components/container/homepage/package-section/standart/StandartPackageSection.tsx
@@ -33,9 +33,7 @@ export default function StandartPackageSection() {
                     <StandartPackageItem 
                         key={i}
                         index={i}
-                        type={data.type}
-                        price={data.price}
-                        packageDetails={data.packageDetails}
+                        {...data}
                     />
                 )}
             </div>
--- src/components/container/homepage/why-us-section/WhyChooseUsSection.tsx
+++ src/components/container/homepage/why-us-section/WhyChooseUsSection.tsx
@@ -23,7 +23,7 @@ export default function WhyChooseUsSection() {
               Why Choose Us?
             </Typography>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-12 md:mt-24">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-16">
             {whyUsData.map((data, i) => 
               <div 
                 key={i}
