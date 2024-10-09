df9e67aa9d1df410dacf5bf994060fd1d92aaf85
nmFaizz <ilznorz582@gmail.com>
2024-10-09 19:42:09 +0700

    fix: module error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import WhyChooseUsSection from "@/components/container/homepage/why-us-section/WhyChooseUsSection";
-import HeroSection from "@/components/container/homepage/hero-section/HeroSection"
-import OurServicesSection from "@/components/container/homepage/services-section/OurServicesSection";
-import StandartPackageSection from "@/components/container/homepage/package-section/StandartPackageSection";
-import FlexiblePackageSection from "@/components/container/homepage/package-section/FlexiblePackageSection";
 import FaqSection from "@/components/container/homepage/faq-section/FaqSection";
+import HeroSection from "@/components/container/homepage/HeroSection";
+import OurServicesSection from "@/components/container/homepage/OurServicesSection";
+import FlexiblePackageSection from "@/components/container/homepage/package-section/FlexiblePackageSection";
+import StandartPackageSection from "@/components/container/homepage/package-section/StandartPackageSection";
+import WhyChooseUsSection from "@/components/container/homepage/WhyChooseUsSection";
+
 
 export default function Home() {
   return (
